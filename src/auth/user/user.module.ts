e4fc8f3755facaf765bed386af55ab6c61dacf39
joonseokchung <joonseok99@naver.com>
2024-05-26 14:00:11 +0900

    fix: added prisma provider to usermodule

--- src/auth/user/user.module.ts
+++ src/auth/user/user.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
+import { PrismaService } from 'src/prisma.service';
 import { UserService } from './user.service';
 
 @Module({
-  providers: [UserService],
+  providers: [UserService, PrismaService],
   exports: [UserService],
 })
 export class UserModule {}
